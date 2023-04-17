db430750c40cf6beef3ecefeb6f608ea0d0d3648
marikanaumova <marusencia.arz.glam@mail.ru>
2023-04-17 18:47:19 +0300

    ....

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Modalka',
+    closable: true,
+    content: `
+       <h4>Modal is working</h4>
+       <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '400px',
+    footerButtons: [
+        {text: 'OK', type: 'primary', handler() {
+            console.log('Primary btn clicked')
+            }},
+        {text: 'Cancel', type: 'danger', handler() {
+                console.log('Danger btn clicked')
+            }},
+    ]
+})
